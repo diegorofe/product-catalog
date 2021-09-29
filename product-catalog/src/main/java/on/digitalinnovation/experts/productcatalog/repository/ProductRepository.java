928aeede39291c40719bbc453f7acad8e86508ed
Diego <diegorf55@gmail.com>
2021-09-29 00:04:44 -0300

    first commit

---
+++ product-catalog/src/main/java/on/digitalinnovation/experts/productcatalog/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package on.digitalinnovation.experts.productcatalog.repository;
+
+
+import on.digitalinnovation.experts.productcatalog.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+}
